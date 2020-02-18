914b14a6f619c9ba0dc676dd4f3e0a7128104c8e
Isfar Baset <baseti@mail.gvsu.edu>
2020-02-18 15:18:58 -0500

    Keyboard updated. Homework 4 complete.

--- HW3-Solution/DecimalMinusTextField.swift
+++ HW3-Solution/DecimalMinusTextField.swift
@@ -24,20 +24,6 @@ class DecimalMinusTextField: UITextField {
         self.keyboardType = UIKeyboardType.decimalPad
         
         
-        self.tintColor = FOREGROUND_COLOR
-        self.layer.borderWidth = 1.0
-        self.layer.borderColor = FOREGROUND_COLOR.cgColor
-        self.layer.cornerRadius = 5.0
-        
-        self.textColor = FOREGROUND_COLOR
-        self.backgroundColor = UIColor.clear
-        self.borderStyle = .roundedRect
-        
-        guard let ph = self.placeholder else{
-            return
-        }
-        
-        self.attributedPlaceholder = NSAttributedString(string: ph, attributes: [NSAttributedString.Key.foregroundColor : FOREGROUND_COLOR])
 
     }
     
